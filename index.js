d909b60d6f45ac533a6e4dc6985d1c9f5930b955
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-17 08:28:57 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(num1 = 2000, num2 = 100) {
+    return num1 / num2
+}
+const square = (para1) => para1 ** 2
+square(5)
+const add = (parameter1 , parameter2) => parameter1 + parameter2
+add(9+2)
